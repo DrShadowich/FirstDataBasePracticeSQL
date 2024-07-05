4f859b9234266f9f0a1b4a3e929fa4086116a885
DrShadowich <martinmaincraft@gmail.com>
2024-07-05 18:29:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace SQL_test
+{
+    internal class Program
+    {
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString;
+
+        private static SqlConnection _sqlConnection = null;
+        
+        static void Main(string[] args)
+        {
+            _sqlConnection = new SqlConnection(_connectionString);
+            _sqlConnection.Open();
+
+            Console.WriteLine("Welcome to my first console app with database.");
+
+            SqlDataReader sqlDataReader = null;
+
+            string command = string.Empty;
+
+            while (true)
+            {
+                try {
+                    Console.Write("> ");
+                    command = Console.ReadLine();
+                    #region EXIT
+                    if (command.ToLower().Equals("exit"))
+                    {
+                        if (_sqlConnection.State == System.Data.ConnectionState.Open) _sqlConnection.Close();
+
+                        sqlDataReader?.Close();
+
+                        break;
+                    }
+                    #endregion
+
+                    SqlCommand sqlCommand = new SqlCommand(command, _sqlConnection);
+
+                    switch (command.Split(' ')[0].ToLower())
+                    {
+                        case "select":
+                            sqlDataReader = sqlCommand.ExecuteReader();
+
+                            while (sqlDataReader.Read())
+                            {
+                                Console.WriteLine($"{sqlDataReader["Name"]} {sqlDataReader["BirthDay"]} {sqlDataReader["Status"]}");
+                                Console.WriteLine(new string('-', 30));
+                            }
+                            sqlDataReader?.Close();
+                            break;
+
+                        case "insert":
+
+                            Console.WriteLine($"Added: {sqlCommand.ExecuteNonQuery()} strings");
+
+                            break;
+
+                        case "update":
+                            Console.WriteLine($"Updated: {sqlCommand.ExecuteNonQuery()} strings");
+
+                            break;
+
+                        case "delete":
+                            Console.WriteLine($"Deleted: {sqlCommand.ExecuteNonQuery()} strings");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Command");
+                            break;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("Enter any button...");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("SQL test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQL test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5ff3b64c-8ebe-4528-8f64-8d8e4c843714")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
